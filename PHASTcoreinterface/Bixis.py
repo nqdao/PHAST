17d9d2656592e518415711283c4999e9e348b5ae
Quang <nhat.quang.d@gmail.com>
2015-10-10 21:26:35 -0400

    merged with polling bixis

--- PHASTcoreinterface/Bixis.py
+++ PHASTcoreinterface/Bixis.py
@@ -7,87 +7,107 @@ import time
 
 
 class Bixis:
-    
-	STATIONS_FILE = "stations.json"
 
-	def __init__(self):	
-		self.routes = {}	
-		self.CVST = CVSTInterface.CVSTInterface()
-		self.stations = []
+    STATIONS_FILE = "stations.json"
 
-		# open up and store the stations if the db file containing their general information
-		# already exists.
-		# should the db file not exist, build it.
-		
-		# if os.path.isfile(self.STATIONS_FILE):
-		with open(self.STATIONS_FILE) as data_file:
-			self.stations = json.load(data_file)
-		# else:
-		# 	self.populate_station_locations()
-		# 	with open(self.STATIONS_FILE,'w') as outfile:
-		# 		json.dump(self.stations, outfile, indent=4, sort_keys=True)
+    def __init__(self):
+        self.routes = {}
+        self.CVST = CVSTInterface.CVSTInterface()
+        self.stations = []
 
-	def populate_station_locations(self):
-		temp_stations = self.CVST.get_all_current_stations()
-		for station in temp_stations:
-			# determine lat and long (note that this will only work for locations
-			# in both the north and western hemispheres)
-			print "working on station {}".format(station["station_id"])
-			if station["coordinates"][0] < 0:
-				lat = station["coordinates"][1]
-				lng = station["coordinates"][0]
-			else:
-				lng = station["coordinates"][1]
-				lat = station["coordinates"][0]
+        # open up and store the stations if the db file containing their general information
+        # already exists.
+        # should the db file not exist, build it.
 
-			del station["coordinates"]
-			station["coordinates"] = {"lat":lat, "lng":lng}
+        # if os.path.isfile(self.STATIONS_FILE):
+        with open(self.STATIONS_FILE) as data_file:
+            self.stations = json.load(data_file)
+            # else:
+            # 	self.populate_station_locations()
+            # 	with open(self.STATIONS_FILE,'w') as outfile:
+            # 		json.dump(self.stations, outfile, indent=4, sort_keys=True)
 
-			station["max_docks"] = self.CVST.get_maximum_docks(station["station_id"])
-			self.stations.append(station)
+    def populate_station_locations(self):
+        temp_stations = self.CVST.get_all_current_stations()
+        for station in temp_stations:
+            # determine lat and long (note that this will only work for locations
+            # in both the north and western hemispheres)
+            print "working on station {}".format(station["station_id"])
+            if station["coordinates"][0] < 0:
+                lat = station["coordinates"][1]
+                lng = station["coordinates"][0]
+            else:
+                lng = station["coordinates"][1]
+                lat = station["coordinates"][0]
 
-	def get_closest_stations(self,location, number_of_stations):
-		closest = []
-		for station in self.stations:
-			distance = self.get_distance(location,station["coordinates"])
-			if len(closest) < number_of_stations:
-				closest.append({"station_id": station["station_id"], "distance": distance, 
-					"coordinates": station["coordinates"]})
-			else:
-				max_location = 0
-				index = 0
-				for close_station in closest:
-					if close_station["distance"] > closest[max_location]["distance"]:
-						max_location = index
-					index = index + 1
+            del station["coordinates"]
+            station["coordinates"] = {"lat": lat, "lng": lng}
 
-				if distance < closest[max_location]["distance"]:
-					del closest[max_location]
-					closest.append({"station_id": station["station_id"], "distance": distance, 
-						"coordinates": station["coordinates"]})
+            station["max_docks"] = self.CVST.get_maximum_docks(station["station_id"])
+            self.stations.append(station)
 
-		return closest
+    def get_closest_stations(self, location, number_of_stations):
+        closest = []
+        for station in self.stations:
+            distance = self.get_distance(location, station["coordinates"])
+            if len(closest) < number_of_stations:
+                closest.append({"station_id": station["station_id"], "distance": distance,
+                                "coordinates": station["coordinates"]})
+            else:
+                max_location = 0
+                index = 0
+                for close_station in closest:
+                    if close_station["distance"] > closest[max_location]["distance"]:
+                        max_location = index
+                    index = index + 1
 
-	def get_distance(self, start, dest):
-		# print start
-		# print dest
-		return math.sqrt(((start["lat"] - dest["lat"])**2 + (start["lng"]-dest["lng"])**2))
+                if distance < closest[max_location]["distance"]:
+                    del closest[max_location]
+                    closest.append({"station_id": station["station_id"], "distance": distance,
+                                    "coordinates": station["coordinates"]})
 
-	def print_stations(self):
-		for station in self.CVST.stations:
-			print "station {0}: ({1},{2})".format(station["station_id"],
-				station["coordinates"]["lat"],station["coordinates"]["lng"])
+        return closest
 
-	def sort_stations(self, stations_list):
-		pass
+    def get_distance(self, start, dest):
+        # print start
+        # print dest
+        return math.sqrt(((start["lat"] - dest["lat"]) ** 2 + (start["lng"] - dest["lng"]) ** 2))
 
+    def print_stations(self):
+        for station in self.CVST.stations:
+            print "station {0}: ({1},{2})".format(station["station_id"],
+                                                  station["coordinates"]["lat"], station["coordinates"]["lng"])
 
-# def main():
-# 	bixis = BixiRoutes()
-# 	closest = bixis.get_closest(UofT_location,7)
-# 	for station in closest:
-# 		print "station {0}:\n--------------\nlocation: ({1},{2})\ndistance: {3}\n".format(
-# 			station["id"],station["coordinates"][0], station["coordinates"][1],station["distance"])
+    def sort_stations(self, stations_list):
+        pass
 
-# if __name__ == "__main__":
-# 	main()
+    # def main():
+    # 	bixis = BixiRoutes()
+    # 	closest = bixis.get_closest(UofT_location,7)
+    # 	for station in closest:
+    # 		print "station {0}:\n--------------\nlocation: ({1},{2})\ndistance: {3}\n".format(
+    # 			station["id"],station["coordinates"][0], station["coordinates"][1],station["distance"])
+
+    # if __name__ == "__main__":
+    # 	main()
+
+if __name__ == '__main__':
+    bixis = Bixis()
+    bixis.populate_station_locations()
+    while True:
+        updated_station_list = bixis.CVST.get_all_current_stations()
+        for updated_station in updated_station_list:
+            for station in bixis.stations:
+                if updated_station["station_id"] == station["station_id"]:
+                    print "updated_station ", updated_station["station_id"], "station", station["station_id"]
+                    for key in updated_station:
+                        if key == "coordinates":
+                            station["coordinates"]["lat"] = updated_station["coordinates"][1]
+                            station["coordinates"]["lng"] = updated_station["coordinates"][0]
+                        else:
+                            station[key] = updated_station[key]
+
+        print "updating CVST data"
+        with open(bixis.STATIONS_FILE, 'w') as outfile:
+            json.dump(bixis.stations, outfile, indent=4, sort_keys=True)
+        time.sleep(10)
