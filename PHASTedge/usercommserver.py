00ae3d50f70cfa1429c1c7721a146e6dfb9e9600
yolande <yolandecates@gmail.com>
2015-10-14 13:55:09 -0400

    refactored user comm server

--- PHASTedge/usercommserver.py
+++ PHASTedge/usercommserver.py
@@ -2,13 +2,14 @@ __author__ = 'Daniel'
 import socket
 import json
 import math
+import User
 
 HOST, PORT, CORE = '', 6633, '10.12.8.3'
 
 class UserCommServer:	 	 
 	
-	routes, location, dest_bixi, new_route, send_new_route = '', '','', '', False
-	  	
+	userlist = [None]*20
+	 	
 	def __init__(self):
 		self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -27,19 +28,22 @@ class UserCommServer:
 
 	def process_incoming(self,received_json):
 		json_resp			
+		details = received_json['details']
+		user_id = details['user_id']
 		if received_json['action'] == 'new_trip':
 			#forward to core, await reply, return routes metadata
 			reply = coreComm(received_json)
 			json_resp = {}
 			json_resp['action'] = 'routes'
-
 			if reply['action'] == 'routes':
-				routes = reply['details']	
-				new_options = reply['details']	 
+				reply_details = reply['details']
+				user_id = reply_details['user_id']
+				userlist[user_id] = User.User(reply_details['routes'])				
+				new_options = reply_details['routes'] 
 				for option in new_options:
 					del option['distance']
 					del option['steps']
-				json_resp['details'] = new_options
+				json_resp['details'] = { 'user_id' : user_id, 'routes': new_options }#??
 			#else invalid	
 
 		elif received_json['action'] == 'selection':
@@ -47,42 +51,42 @@ class UserCommServer:
 			reply = coreComm(received_json)
 			json_resp = {}
 			json_resp['action'] = 'route_info'
-			rte_selected = routes[received_json['selection']]
+			rte_selected = userlist[user_id].routes[details['selection']]
 			steps = rte_selected['steps']		
 			json_resp['details'] = 	steps
 			last_bike_step = steps[steps.length-1]
-			dest_bixi = last_bike_step['end_location']
+			userlist[user_id].dest_bixi = last_bike_step['end_location']
 
 		elif received_json['action'] == 'location':
 			json_resp = {}
-			location = received_json['location']
-			if checkDone():
+			userlist[user_id].location = received_json['location']
+			if checkDone(user_id):
 				json_resp['action'] = 'done'
 				json_resp['details'] = ''				
 				reply = coreComm(json_resp)
 			elif send_new_route:
 				json_resp['action'] = 'new_route'
-				json_resp['details'] = new_route
-				send_new_route = False				
+				json_resp['details'] = userlist[user_id].new_route
+				userlist[user_id].send_new_route = False				
 			else:
 				json_resp['action']  = 'ack'
 				json_resp['details'] = ''
 
 		elif received_json['action'] == 'new_route':
 			#store new route details, forward to user at next location update
-			send_new_route = True
-			new_route = received_json['options']			
+			userlist[user_id].send_new_route = True
+			userlist[user_id].new_route = received_json['options']			
 
 		elif received_json['action'] == 'get_location':
 			#send location to core
 			json_resp['action'] = 'location'
-			json_resp['details'] = location
+			json_resp['details'] = userlist[user_id].location
 		#else ack or invalid 
 
 		return json_resp
 
-	def checkDone():
-		latlong_dist = math.sqrt( (location["lat"] - dest_bixi["lat"]) ** 2 + (location["lng"] - dest_bixi["lng"]) ** 2 )
+	def checkDone(self,userid):
+		latlong_dist = math.sqrt( (userlist[user_id].location["lat"] - userlist[user_id].dest_bixi["lat"]) ** 2 + (userlist[user_id].location["lng"] - userlist[user_id].dest_bixi["lng"]) ** 2 )
 		#0.00045 decimal degrees ~50m, assuming flat earth approximation for very small distances
 		return latlong_dist < 0.00045 
 		
