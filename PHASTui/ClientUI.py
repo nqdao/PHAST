d696942043b6c875e3132eded3d212d3c0ecfb72
Quang <nhat.quang.d@gmail.com>
2015-10-15 02:26:35 -0400

    draw selected route to map, set button for simulating movement, allows up to 4 choices of routes

--- PHASTui/ClientUI.py
+++ PHASTui/ClientUI.py
@@ -3,17 +3,23 @@ __author__ = 'Nhat-Quang'
 import wx, os
 import wx.html2
 import urllib
-import json, socket, sys
+import json, socket, copy
 
 
 class Gui(wx.Frame):
     HOST, PORT = "142.150.208.139", 6633
     ORIGIN = "University of Toronto"
     LOCATION_LIST = [(0, 0), (1, 1), (2, 2), (10, 10)]
+    INITIAL_URL = "https://maps.googleapis.com/maps/api/staticmap?center=CN+Tower,Toronto,ON&zoom=14&size=800x600&maptype=roadmap"
+    ROUTE_URL = "https://maps.googleapis.com/maps/api/staticmap?size=800x600"
+    API_KEY = "&key=AIzaSyA7LGD3TmN87_-Lr8sofhjaylZK1q87ra8"
+    PATH_TEMPLATE = ["&path=weight:10%7Ccolor:", "%7Cenc:"]
+    MARKER_TEMPLATE = ["&markers=color:", "%7C"]
+    steps = []
+    iterator = 0    # iterate through steps list to keep track of where user is along the route
 
     def __init__(self, parent, title):
-        super(Gui, self).__init__(parent, title=title,
-            size=(1000, 750))
+        super(Gui, self).__init__(parent, title=title, size=(1280, 750))
 
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -32,20 +38,37 @@ class Gui(wx.Frame):
         input_box.Add(self.location_box, flag=wx.LEFT | wx.TOP | wx.RIGHT, border=10)
         input_box.Add(self.comp_button, flag=wx.LEFT | wx.TOP, border=10)
 
+        #TODO: this button is only here for SAVI design cmap demo purposes
+        self.proceed_button = wx.Button(panel, label="Proceed to next step", size=(150, 50))
+        self.proceed_button.Bind(wx.EVT_BUTTON, self.demo_monitor_communication)
+        input_box.Add(self.proceed_button, flag=wx.LEFT | wx.TOP, border=10)
+        self.instruction = wx.StaticText(panel, label="Instruction here")
+        input_box.Add(self.instruction, flag=wx.LEFT | wx.TOP, border=10)
+
+
         img_box = wx.BoxSizer(wx.HORIZONTAL)
         self.img = wx.StaticBitmap(panel, -1, wx.Bitmap(self.img_file, wx.BITMAP_TYPE_ANY))
         img_box.Add(self.img, flag=wx.LEFT | wx.BOTTOM | wx.RIGHT | wx.EXPAND, border=10)
 
         radio_box = wx.BoxSizer(wx.VERTICAL)
-        self.route_1 = wx.Button(panel, label='Choose Bixi')
-        self.route_2 = wx.Button(panel, label='Choose Bus')
-        self.route_1_desc = wx.StaticText(panel, label="Bixi route description")
-        self.route_2_desc = wx.StaticText(panel, label="Bus route description")
+        self.route_1 = wx.Button(panel, label='Choose 1')
+        self.route_2 = wx.Button(panel, label='Choose 2')
+        self.route_3 = wx.Button(panel, label='Choose 3')
+        self.route_4 = wx.Button(panel, label='Choose 4')
+        self.route_1_desc = wx.StaticText(panel, label="Route 1 description")
+        self.route_2_desc = wx.StaticText(panel, label="Route 2 description")
+        self.route_3_desc = wx.StaticText(panel, label="Route 3 description")
+        self.route_4_desc = wx.StaticText(panel, label="Route 4 description")
+
 
         radio_box.Add(self.route_1_desc, flag=wx.RIGHT | wx.TOP, border=10)
-        radio_box.Add(self.route_1, flag=wx.RIGHT | wx.TOP, border=50)
+        radio_box.Add(self.route_1, flag=wx.RIGHT | wx.TOP, border=30)
         radio_box.Add(self.route_2_desc, flag=wx.RIGHT | wx.TOP, border=10)
-        radio_box.Add(self.route_2, flag=wx.RIGHT | wx.TOP, border=40)
+        radio_box.Add(self.route_2, flag=wx.RIGHT | wx.TOP, border=30)
+        radio_box.Add(self.route_3_desc, flag=wx.RIGHT | wx.TOP, border=10)
+        radio_box.Add(self.route_3, flag=wx.RIGHT | wx.TOP, border=30)
+        radio_box.Add(self.route_4_desc, flag=wx.RIGHT | wx.TOP, border=10)
+        radio_box.Add(self.route_4, flag=wx.RIGHT | wx.TOP, border=30)
 
         img_box.Add(radio_box, flag=wx.RIGHT, border=10)
 
@@ -54,7 +77,7 @@ class Gui(wx.Frame):
         sizer.Add(img_box, 1, wx.EXPAND | wx.LEFT | wx.BOTTOM, 20)
 
         panel.SetSizer(sizer)
-
+        self.display_map()
         self.Center()
         self.Show()
 
@@ -64,24 +87,26 @@ class Gui(wx.Frame):
             return
         destination = self.location_box.GetValue()
         print destination
-        self.s.connect((self.HOST, self.PORT))
-        self.s.sendall(json.dumps(self.construct_new_trip_request(destination)))
+        # self.s.connect((self.HOST, self.PORT))
+        # self.s.sendall(json.dumps(self.construct_new_trip_request(destination)))
+        #
+        # # #receive routes from comm
+        # route_selections = json.loads(self.s.recv(1024))
+        with open("test_bixi_route.json") as test:
+            routes = json.load(test)
+            print self.route_to_url(routes)
+            self.display_map(self.route_to_url(routes))
+        # if route_selections['action'] != 'routes':
+        #     print "ERROR: expecting routes from server"
+        #     return
 
-        # #receive routes from comm
-        route_selections = json.loads(self.s.recv(1024))
-        with open("test_routes.json") as test:
-            route_selections = json.load(test)
-        if route_selections['action'] != 'routes':
-            print "ERROR: expecting routes from server"
-            return
 
         #display and let user select route
-        self.route_options(route_selections['details']['routes'])
-        self.display_map()
+        # self.route_options(route_selections['details']['routes'])
 
-    def display_map(self):
+    def display_map(self, url="{0}{1}".format(INITIAL_URL, API_KEY)):
         if os.path.isfile(self.img_file):
-            urllib.urlretrieve(self.url, self.img_file)
+            urllib.urlretrieve(url, self.img_file)
             self.img.SetBitmap(wx.Bitmap(self.img_file, wx.BITMAP_TYPE_ANY))
         self.img.Refresh()
 
@@ -97,21 +122,93 @@ class Gui(wx.Frame):
                               "duration: {2}".format("Bus", routes['bus']['distance'], routes['bus']['duration']))
         self.route_2.Bind(wx.EVT_BUTTON, lambda event: self.construct_route_selection(event, "bus"))
 
+    def route_to_url(self, route_info):
+        self.steps = route_info['steps']
+        url = copy.copy(self.ROUTE_URL)
+        for i in range(len(self.steps)):
+            if self.steps[i]['travel_mode'] == "WALKING":
+                if i == 0:
+                    # make start marker at this point GREEN
+                   url += "{0}{1}{2}{3},{4}".format(self.MARKER_TEMPLATE[0], "green", self.MARKER_TEMPLATE[1],
+                                                   self.steps[i]['start_location']['lat'],
+                                                   self.steps[i]['start_location']['lng'])
+                elif self.steps[i-1]['travel_mode'] == "BICYCLING" or self.steps[i-1]['travel_mode'] == "TRANSIT":
+                    # make bike/bus destination marker BLUE
+                    url += "{0}{1}{2}{3},{4}".format(self.MARKER_TEMPLATE[0], "blue", self.MARKER_TEMPLATE[1],
+                                                   self.steps[i-1]['end_location']['lat'],
+                                                   self.steps[i-1]['end_location']['lng'])
+                elif i == len(self.steps) - 1:
+                    # make final destination marker RED
+                    url += "{0}{1}{2}{3},{4}".format(self.MARKER_TEMPLATE[0], "red", self.MARKER_TEMPLATE[1],
+                                                   self.steps[i]['end_location']['lat'],
+                                                   self.steps[i]['end_location']['lng'])
+
+                #make a path color BLUE
+                url += "{0}{1}{2}{3}".format(self.PATH_TEMPLATE[0], "blue", self.PATH_TEMPLATE[1],
+                                          self.steps[i]['polyline']['points'])
+            elif self.steps[i]['travel_mode'] == "BICYCLING":
+                if self.steps[i-1]['travel_mode'] == "WALKING":
+                    # make start bike marker BLUE
+                    url += "{0}{1}{2}{3},{4}".format(self.MARKER_TEMPLATE[0], "blue", self.MARKER_TEMPLATE[1],
+                                                     self.steps[i]['start_location']['lat'],
+                                                     self.steps[i]['start_location']['lng'])
+
+                # make a path color ORANGE
+                url += "{0}{1}{2}{3}".format(self.PATH_TEMPLATE[0], "orange", self.PATH_TEMPLATE[1],
+                                          self.steps[i]['polyline']['points'])
+            elif self.steps[i]['travel_mode'] == "TRANSIT":
+                if self.steps[i-1]['travel_mode'] == "WALKING":
+                    # make start bike marker BLUE
+                    url += "{0}{1}{2}{3},{4}".format(self.MARKER_TEMPLATE[0], "blue", self.MARKER_TEMPLATE[1],
+                                                     self.steps[i]['start_location']['lat'],
+                                                     self.steps[i]['start_location']['lng'])
+
+                # make a path color RED
+                url += "{0}{1}{2}{3}".format(self.PATH_TEMPLATE[0], "red", self.PATH_TEMPLATE[1],
+                                             self.steps[i]['polyline']['points'])
+            else:
+                print "Transportation mode not supported"
+                return None
+
+        url += self.API_KEY
+        print url
+        return url
+
     def monitor_communication(self):
         data_in = json.loads(self.s.recv(1024))
         while data_in['action'] != "done":
-            if data_in['action'] == 'new_route':
-                #update map to show new route
-                self.display_map()
+            if data_in['action'] == 'route_info':
+                # update map to show selected route
+                self.display_map(self.route_to_url(data_in['details']['route']))
+            elif data_in['action'] == 'new_route':
+                # update to show new route
+                self.display_map(self.route_to_url(data_in['details']['route']))
 
-            self.s.sendall(json.dumps(self.construct_location_response()))
+            self.s.sendall(json.dumps(self.construct_location_response(self.iterator)))
             data_in = json.loads(self.s.recv(1024))
 
         print json.dumps(data_in)
         wx.MessageBox('You have arrived at your destination', 'Arrived', wx.OK | wx.ICON_EXCLAMATION)
         self.s.close()
 
-    @staticmethod
+    def demo_monitor_communication(self, event):
+        self.instruction.SetLabel(self.steps[self.iterator]['html_instructions'])
+        self.s.sendall(json.dumps(self.construct_location_response(self.iterator + 1)))
+        data_in = json.loads(self.s.recv(1024))
+        if data_in['action'] == 'route_info':
+            # update map to show selected route
+            self.display_map(self.route_to_url(data_in['details']['route']))
+        elif data_in['action'] == 'new_route':
+            # update to show new route
+            self.display_map(self.route_to_url(data_in['details']['route']))
+        elif data_in['action'] == 'done':
+            wx.MessageBox('You have arrived at your destination', 'Arrived', wx.OK | wx.ICON_EXCLAMATION)
+            self.s.close()
+            self.iterator = 0
+            return
+
+        self.iterator += 1
+
     def construct_new_trip_request(self, destination, origin=ORIGIN):
         data_out = {'action': "new_trip", 'details': {"user_id": 0, "origin": origin, "destination": destination}}
         return data_out
@@ -123,10 +220,9 @@ class Gui(wx.Frame):
         self.monitor_communication()
 
 
-    @staticmethod
     def construct_location_response(self, i):
-        data_out = {'action': "location", 'details': {'user_id': 0, 'location': {'lat': self.LOCATION_LIST[i][0],
-                                                                                 'lng': self.LOCATION_LIST[i][1]}}}
+        data_out = {'action': "location", 'details': {'user_id': 0, 'location': {'lat': self.steps[i]['start_location']['lat'],
+                                                                                 'lng': self.steps[i]['start_location']['lng']}}}
         return data_out
 
 if __name__ == '__main__':
